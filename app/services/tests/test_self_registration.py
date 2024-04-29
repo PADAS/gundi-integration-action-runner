4784003162760470d032a25a14394f50a91c422a
Mariano Martinez Grasso <marianom@earthranger.com>
2024-04-29 11:46:18 -0300

    Improve test coverage in self-registration

--- app/services/tests/test_self_registration.py
+++ app/services/tests/test_self_registration.py
@@ -1,32 +1,116 @@
 import pytest
-from unittest.mock import patch
 from fastapi.testclient import TestClient
 from app.main import app
 from app.services.self_registration import register_integration_in_gundi
-
 
 api_client = TestClient(app)
 
 
 @pytest.mark.asyncio
-async def test_register_integration_with_slug_setting(mocker, mock_gundi_client_v2):
+async def test_register_integration_with_slug_setting(mocker, mock_gundi_client_v2, mock_action_handlers):
     mocker.patch("app.services.self_registration.INTEGRATION_TYPE_SLUG", "x_tracker")
+    mocker.patch("app.services.self_registration.action_handlers", mock_action_handlers)
     await register_integration_in_gundi(gundi_client=mock_gundi_client_v2)
     assert mock_gundi_client_v2.register_integration_type.called
+    mock_gundi_client_v2.register_integration_type.assert_called_with(
+        {
+            "name": "X Tracker",
+            "value": "x_tracker",
+            "description": f"Default type for integrations with X Tracker",
+            "actions": [
+                {
+                    'description': 'X Tracker Pull Observations action',
+                    'is_periodic_action': True,
+                    'name': 'Pull Observations',
+                    'schema': {
+                        'properties': {
+                            'lookback_days': {
+                                'default': 10,
+                                'title': 'Lookback Days',
+                                'type': 'integer'
+                            }
+                        },
+                        'title': 'MockPullActionConfiguration',
+                        'type': 'object'
+                    },
+                    'type': 'pull',
+                    'value': 'pull_observations'
+                }
+            ]
+        }
+    )
 
 
 @pytest.mark.asyncio
-async def test_register_integration_with_slug_arg(mock_gundi_client_v2):
+async def test_register_integration_with_slug_arg(mocker, mock_gundi_client_v2, mock_action_handlers):
+    mocker.patch("app.services.action_runner.action_handlers", mock_action_handlers)
+    mocker.patch("app.services.self_registration.action_handlers", mock_action_handlers)
     await register_integration_in_gundi(gundi_client=mock_gundi_client_v2, type_slug="x_tracker")
     assert mock_gundi_client_v2.register_integration_type.called
+    mock_gundi_client_v2.register_integration_type.assert_called_with(
+        {
+            "name": "X Tracker",
+            "value": "x_tracker",
+            "description": f"Default type for integrations with X Tracker",
+            "actions": [
+                {
+                    'description': 'X Tracker Pull Observations action',
+                    'is_periodic_action': True,
+                    'name': 'Pull Observations',
+                    'schema': {
+                        'properties': {
+                            'lookback_days': {
+                                'default': 10,
+                                'title': 'Lookback Days',
+                                'type': 'integer'
+                            }
+                        },
+                        'title': 'MockPullActionConfiguration',
+                        'type': 'object'
+                    },
+                    'type': 'pull',
+                    'value': 'pull_observations'
+                }
+            ]
+        }
+    )
 
 
 @pytest.mark.asyncio
-async def test_register_integration_with_slug_service_url(mocker, mock_gundi_client_v2):
+async def test_register_integration_with_slug_service_url(mocker, mock_gundi_client_v2, mock_action_handlers):
     mocker.patch("app.services.self_registration.INTEGRATION_TYPE_SLUG", "x_tracker")
+    mocker.patch("app.services.self_registration.action_handlers", mock_action_handlers)
+    service_url = "https://xtracker-actions-runner-jabcutl8yb-uc.a.run.app"
     await register_integration_in_gundi(
         gundi_client=mock_gundi_client_v2,
-        service_url="https://xtracker-actions-runner-jabcutl8yb-uc.a.run.app"
+        service_url=service_url
     )
     assert mock_gundi_client_v2.register_integration_type.called
-
+    mock_gundi_client_v2.register_integration_type.assert_called_with(
+        {
+            "name": "X Tracker",
+            "value": "x_tracker",
+            "description": f"Default type for integrations with X Tracker",
+            'service_url': service_url,
+            "actions": [
+                {
+                    'description': 'X Tracker Pull Observations action',
+                    'is_periodic_action': True,
+                    'name': 'Pull Observations',
+                    'schema': {
+                        'properties': {
+                            'lookback_days': {
+                                'default': 10,
+                                'title': 'Lookback Days',
+                                'type': 'integer'
+                            }
+                        },
+                        'title': 'MockPullActionConfiguration',
+                        'type': 'object'
+                    },
+                    'type': 'pull',
+                    'value': 'pull_observations'
+                }
+            ]
+        }
+    )
