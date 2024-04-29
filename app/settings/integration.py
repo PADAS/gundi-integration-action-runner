fa7ddbe7b608aba88722baafb74a2c90152c35b3
Mariano Martinez Grasso <marianom@earthranger.com>
2024-04-29 11:03:07 -0300

    Allow defining the integration type through env vars

--- app/settings/integration.py
+++ app/settings/integration.py
@@ -1,2 +1,4 @@
+from .base import *
+
 # Add your integration-specific settings here
-INTEGRATION_TYPE_SLUG = "dummy_type"  # e.g. "my_tracker"
+INTEGRATION_TYPE_SLUG = env.str("INTEGRATION_TYPE_SLUG", None)  # Define a string id here e.g. "my_tracker"
