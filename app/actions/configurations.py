cdbd1b81f6404eb82910060e84cd8d33c3737a2f
Victor H. García <victorhugogarcia13d@gmail.com>
2024-05-10 15:02:52 -0600

    GFW v2 MK 1

--- app/actions/configurations.py
+++ app/actions/configurations.py
@@ -1,1 +1,13 @@
-from .core import ActionConfiguration
+from .core import PullActionConfiguration, AuthActionConfiguration
+
+
+class AuthenticateConfig(AuthActionConfiguration):
+    email: str
+    password: str
+
+
+class PullEventsConfig(PullActionConfiguration):
+    carto_url: str = "https://rw-nrt.carto.com:443/api/v2/sql"
+    url: str
+    include_fire_alerts: bool = True
+    include_tree_losses_alerts: bool = True
