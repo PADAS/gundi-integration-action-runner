514d32528c307f988f0c020511e6fa1bd5c1cc83
jeslefcourt <32883811+jeslefcourt@users.noreply.github.com>
2024-11-14 15:31:19 -0800

    Significant updates after getting things connected for live testing.

--- app/actions/configurations.py
+++ app/actions/configurations.py
@@ -1,4 +1,5 @@
 from typing import Optional, List, Dict
+import json
 import pydantic
 from .core import PullActionConfiguration, AuthActionConfiguration, ExecutableActionMixin
 
@@ -18,20 +19,17 @@ class PullEventsConfig(PullActionConfiguration):
     days_to_load: int = pydantic.Field(30, title = "Default number of days to load",
         description="The number of days of data to load from iNaturalist.  If the integration state contains a last_run value, this parameter will be ignored and data will be loaded since the last_run value.")
 
-    bounding_box: Optional[List[float]] = pydantic.Field(title = "Bounding box for search area.  Of the format [ne_latitude, ne_longitude, sw_latitude, sw_longitude]")
+    bounding_box: Optional[str] = pydantic.Field(title = "Bounding box for search area.  Of the format [ne_latitude, ne_longitude, sw_latitude, sw_longitude]")
 
     projects: Optional[List[int]] = pydantic.Field(title = "Project IDs", 
         description="List of project IDs to pull from iNaturalist.")
     
-    taxa: Optional[List[int]] = pydantic.Field(title = "Taxa IDs", 
+    taxa: Optional[List[str]] = pydantic.Field(title = "Taxa IDs", 
         description="List of iNaturalist taxa IDs for which to load observations.")
     
-    annotations: Optional[Dict[int,List[int]]] = pydantic.Field(title = "Annotations",
-        description="Map of annotation terms and the values which to include.  For example, 22: [24, 25] would only include observations that had the Evidence of Presence annotation set to Organism or Scat.  Entries in the Dict are treated as ORs, whereas values in the Lists are treated as ANDs.")
+    annotations: Optional[str] = pydantic.Field(title = "Annotations",
+        description='Map of annotation terms and the values which to include.  For example, {"22": ["24", "25"], "1": ["2"]} would only include observations of Adults (annotation 1 == 2) that had the Evidence of Presence annotation (22) set to Organism (24) or Scat (25).  Entries in the Dict are treated as ORs, whereas values in the Lists are treated as ANDs.')
 
-    priorities: Optional[Dict[int,List[int]]] = pydantic.Field(title="Taxa Priorities",
-        description="Dict of priority levels to use for ER events and, for each, a list of taxa whose observations should be given that priority.")
-    
     include_photos: Optional[bool] = pydantic.Field(True, title="Include photos",
         description = "Whether or not to include the photos from iNaturalist observations.  Default: True")
 
@@ -42,10 +40,29 @@ class PullEventsConfig(PullActionConfiguration):
             return None
         return v
 
+    @pydantic.validator("annotations", always=True)
+    def validate_json(cls, v):
+        v = v.strip()
+        if(v == ""):
+            return None
+        try:
+            v = json.loads(v)
+        except:
+            raise ValueError(f"Could not parse json: {v}")
+        return v
+
     @pydantic.validator("bounding_box", always=True)
     def validate_bounding_box(cls, v):
-        if(not v or len(v) != 4):
+        if(not v):
+            raise ValueError("Did not receive a bounding box configuration.")
+        coords = json.loads(v)
+        if(len(coords) != 4):
             raise ValueError("Did not receive four values in bounding box configuration.")
+        for coord in coords:
+            try:
+                float(coord)
+            except:
+                raise ValueError(f"Could not parse bounding box values {v}.")
         return v
 
     class Config:
@@ -58,5 +75,5 @@ class PullEventsConfig(PullActionConfiguration):
                     "num_days_default": 1
                 }
             ],
-            "required": ["latitude", "longitude", "distance", "num_days"]
+            "required": ["bounding_box", "days_to_load"]
         }
--- app/actions/handlers.py
+++ app/actions/handlers.py
@@ -1,21 +1,24 @@
 import httpx
 import logging
+import json
 from datetime import datetime, timezone, timedelta
 from math import ceil
 from app.actions.configurations import AuthenticateConfig, PullEventsConfig
 from app.services.activity_logger import activity_logger
-from app.services.gundi import send_events_to_gundi
+from app.services.gundi import send_events_to_gundi, update_event_in_gundi, send_event_attachments_to_gundi
 from app.services.state import IntegrationStateManager
 from app.services.errors import ConfigurationNotFound, ConfigurationValidationError
 from app.services.utils import find_config_for_action
 from gundi_core.schemas.v2 import Integration
-from pyinaturalist import get_observations, Observation, Annotation
+from pyinaturalist import get_observations_v2, Observation, Annotation
 from pydantic import BaseModel, parse_obj_as
 from typing import Dict, List, Optional
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
 
 import re
+
+GUNDI_SUBMISSION_CHUNK_SIZE = 10
 
 logger = logging.getLogger(__name__)
 state_manager = IntegrationStateManager()
@@ -40,16 +43,26 @@ async def handle_transformed_data(transformed_data, integration_id, action_id):
     else:
         return response
 
-def get_inaturalist_observations(integration, config, since):
+def get_inaturalist_observations(integration: Integration, config: PullEventsConfig, since: datetime):
 
-    bounding_box = integration.get("bounding_box")
     nelat = nelng = swlat = swlng = None
-    if(bounding_box):
-        nelat, nelng, swlat, swlng = bounding_box
+    if(config.bounding_box):
+        nelat, nelng, swlat, swlng = json.loads(config.bounding_box)
 
-    inat_count_req = get_observations(page = 1, per_page = 0, updated_since = since, project_id = config.projects,
-                                      taxon_id=config.taxa, nelat = nelat, nelng = nelng, swlat = swlat, swlng = swlng,
-                                      order_by = "updated_since", order="asc")
+    target_taxa = []
+    for taxa in config.taxa:
+        target_taxa.append(str(taxa))
+    target_taxa = ",".join(target_taxa)
+
+
+    fields = ",".join(["observed_on", "created_at", "id", "captive", "obscured", "place_guess", "quality_grade", "species_guess", "updated_at", 
+                       "uri", "photos", "user", "location", "place_ids", "taxon", "photos.large_url", "photos.url", "taxon.id", "taxon.rank", "taxon.name",
+                       "taxon.preferred_common_name", "taxon.wikipedia_url", "taxon.conservation_status", "user.id", "user.name", "user.login",
+                       "annotations.controlled_attribute_id", "annotations.controlled_value_id"])
+
+    inat_count_req = get_observations_v2(page = 1, per_page = 0, updated_since = since, project_id = config.projects,
+                                      taxon_id=target_taxa, nelat = float(nelat), nelng = float(nelng), swlat = float(swlat), swlng = float(swlng),
+                                      order_by = "updated_at", order="asc")
 
     inat_count = inat_count_req.get("total_results")
     pages = ceil(inat_count/200)
@@ -58,11 +71,13 @@ def get_inaturalist_observations(integration, config, since):
     for page in range(1,pages+1):
         logger.debug(f"Loading page {page} of {pages} from iNaturalist")
 
-        response = get_observations(page=page,per_page=200, updated_since = since, project_id = config.projects,
-                                    taxon_id=config.target_taxa, nelat = nelat, nelng = nelng, swlat = swlat, swlng = swlng,
-                                    order_by = 'updated_since', order="asc")
+        response = get_observations_v2(page = page, per_page = 200, updated_since = since, project_id = config.projects,
+                                    taxon_id = target_taxa, nelat = nelat, nelng = nelng, swlat = swlat, swlng = swlng,
+                                    order_by = 'updated_at', order="asc", fields=fields)
 
         observations = Observation.from_json_list(response)
+
+        logger.info(f"Loaded {len(observations)} observations from iNaturalist before annotation filters.")
         for o in observations:
             if(config.annotations):
                 if(_match_annotations_to_config(o.annotations, config.annotations)):    
@@ -72,28 +87,34 @@ def get_inaturalist_observations(integration, config, since):
 
     return observation_map
 
+def chunk_list(list_a, chunk_size):
+  for i in range(0, len(list_a), chunk_size):
+    yield list_a[i:i + chunk_size]
+
 @activity_logger()
 async def action_pull_events(integration:Integration, action_config: PullEventsConfig):
 
     logger.info(f"Executing 'pull_events' action with integration {integration} and action_config {action_config}...")
 
     state = await state_manager.get_state(integration.id, "pull_events")
+
     last_run = state.get('updated_to') or state.get('last_run')
     load_since = None
     now = datetime.now(tz=timezone.utc)
     if(last_run):
-        load_since = datetime.datetime.strptime(last_run, '%Y-%m-%d %H:%M:%S%z')
+        load_since = datetime.strptime(last_run, '%Y-%m-%d %H:%M:%S%z')
     else:
         load_since = now - timedelta(days=action_config.days_to_load)
 
 
-    observations = get_inaturalist_observations(integration, load_since)
-    logger.info(f"Processing {len(observations)} observations from iNaturalist for query {query}.")
+    observations = get_inaturalist_observations(integration, action_config, load_since)
+    logger.info(f"Processing {len(observations)} observations from iNaturalist.")
     
     all_event_photos = {}
     newest = None
     events_to_process = []
     for ob in observations.values():
+        
         if(not newest or (newest < ob.created_at)):
             newest = ob.created_at
 
@@ -103,49 +124,56 @@ async def action_pull_events(integration:Integration, action_config: PullEventsC
         inat_id = e['event_details']['inat_id']
         all_event_photos[inat_id] = []
         for photo in ob.photos:
-            all_event_photos[inat_id].append((photo.id, photo.large_url))
+            all_event_photos[inat_id].append((photo.id, photo.large_url if photo.large_url else photo.url))
 
     id_map = state.get("inat_to_gundi", {})
-    to_add = {}
-    to_update = {}
-    for e in events_to_process:
-        e_id = e['event_details']['inat_id']
-        if(e_id) in id_map:
-            to_update[e_id] = e
-        else:
-            to_add[e_id] = e
+
+    logger.info(f"Submitting {len(events_to_process)} iNaturalist observations to Gundi")
+    i = 0
+    updated_count = 0
+    added_count = 0
+    attachment_count = 0
+    for to_add_chunk in chunk_list(events_to_process, GUNDI_SUBMISSION_CHUNK_SIZE):
+        
+        if(len(events_to_process) > GUNDI_SUBMISSION_CHUNK_SIZE):
+            i += 1
+            logger.info(f"Processing chunk #{i}")
     
-    logger.info(f"Submitting {len(to_update)} iNaturalist observations for update in Gundi")
-    for e_id, e in to_update.items():
-        await update_event_in_gundi(event_id = e_id, event = e)
+        for e in to_add_chunk:
+            e_id = e['event_details']['inat_id']
+            if(e_id) in id_map: 
+                await update_event_in_gundi(event_id = id_map[e_id], event = e, integration_id=str(integration.id))
+                updated_count += 1
+            
+            else:
+                response = await send_events_to_gundi(events=[e], integration_id=str(integration.id))
+                added_count += 1
+                
+                inat_id = e['event_details']['inat_id']
+                id_map[inat_id] = response[0]['object_id']
 
-    logger.info(f"Submitting {len(to_add)} new iNaturalist observations to Gundi")
-    response = await send_events_to_gundi(events=to_add.values(), integration_id=str(integration.id))
+                if(action_config.include_photos):
+                    for (photo_id, photo_url) in all_event_photos[inat_id]:
+                        gundi_id = response[0]['object_id']
 
-    for (to_add, added) in zip(to_add.values(), response):
-        inat_id = to_add['event_details']['inat_id']
-        id_map[inat_id] = added['object_id']
+                        logger.info(f"Adding {photo_url} from iNat event {inat_id} to Gundi event {gundi_id}")
+                        fp = urlretrieve(photo_url)
+                        path = urlparse(photo_url).path
+                        ext = re.split(r".*\.", path)[1]
+                        filename = str(photo_id) + "." + ext
+                        attachments = [(filename, open(fp[0], 'rb'))]
+                        await send_event_attachments_to_gundi(event_id = gundi_id, attachments = attachments, integration_id=str(integration.id))
+                        attachment_count += 1
 
-        if(action_config.include_photos):
-            for (photo_id, photo_url) in all_event_photos[inat_id]:
-                gundi_id = added['object_id']
+            last_updated = e['event_details']['updated_at']
 
-                logger.info(f"Adding {photo_url} from iNat event {inat_id} to Gundi event {gundi_id}")
-                fp = urlretrieve(photo_url)
-                path = urlparse(photo_url).path
-                ext = re.split(r".*\.", path)[1]
-                filename = str(photo_id) + "." + ext
-                attachments = [(filename, open(fp, 'rb'))]
-                send_event_attachments_to_gundi(gundi_id, attachments)
-
-
-    state = {"last_run": now.strftime('%Y-%m-%d %H:%M:%S%z'),
-             "inat_to_gundi": id_map}
-
-    await state_manager.set_state(str(integration.id), "pull_events", state)
-
-    return {'result': {'events_extracted': len(to_add),
-                       'events_updated': len(to_update)}}
+        logger.info(f"Updating state through {last_updated}")
+        state = {"last_run": last_updated.strftime('%Y-%m-%d %H:%M:%S%z'), "inat_to_gundi": id_map}
+        await state_manager.set_state(str(integration.id), "pull_events", state)
+        
+    return {'result': {'events_extracted': added_count,
+                       'events_updated': updated_count,
+                       'photos_attached': attachment_count}}
 
 def _match_annotations_to_config(annotations: List[Annotation], config: Dict[int, List[int]]):
     annot_map = {}
@@ -155,12 +183,12 @@ def _match_annotations_to_config(annotations: List[Annotation], config: Dict[int
         annot_map[annotation.term].append(annotation.value)
 
     for term, values in config:
-        if(term not in annot_map):
+        if(str(term) not in annot_map):
             return False
         for value in values:
-            if(value not in annot_map[term]):
+            if(str(value) not in annot_map[str(term)]):
                 return False
-            
+    
     return True
 
 
@@ -170,7 +198,7 @@ def _transform_inat_to_gundi_event(ob: Observation, config: PullEventsConfig):
         "event_type": config.event_type,
         "recorded_at": ob.observed_on if ob.observed_on else ob.created_at,
         "event_details": {
-            "inat_id": ob.id,
+            "inat_id": str(ob.id),
             "captive": ob.captive,
             "location_obscured": ob.obscured,
             "created_at": ob.created_at,
@@ -178,16 +206,18 @@ def _transform_inat_to_gundi_event(ob: Observation, config: PullEventsConfig):
             "quality_grade": ob.quality_grade,
             "species_guess": ob.species_guess,
             "updated_at": ob.updated_at,
-            "inat_url": ob.uri,
-            "user_id": ob.user.id,
-            "user_name": ob.user.name
+            "inat_url": ob.uri
         }
     }
 
+    if(ob.user):
+        event['event_details']['user_id'] = ob.user.id
+        event['event_details']['user_name'] = ob.user.name if ob.user.name else ob.user.login
+
     if(ob.location):
         event["location"] = {
-            "latitude": ob.location[0],
-            "longitude": ob.location[1] }
+            "lat": ob.location[0],
+            "lon": ob.location[1] }
 
     if(ob.place_ids):
         event["event_details"]["place_ids"] = ",".join([str(int) for int in ob.place_ids])
