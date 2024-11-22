fa073605c62bf8cce4c8e1b46063518b14054037
Halley Weitzman <halleyw@earthranger.com>
2024-11-22 14:41:23 -0800

    -Add unit tests: not tested locally. VsCode pytest setup not working

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,416 @@
 import pytest
 
+from datetime import datetime, timedelta, timezone
+import json
+
+from ropeless_utils import State
+from cdip_connector.core.schemas import GeoEvent, Location, IntegrationInformation
+
 # TODO: Add a fixture for testing the RmwHubAdapter
+
+
+@pytest.fixture
+def a_good_state():
+    return State(
+        er_token="super_secret_token",
+        er_site="fishing.pamdas.org",
+        event_source="fancy_buoy_company",
+        er_event_type="gear_position",
+        er_buoy_config=State(
+            er_token="an_er_buoy_site_token",
+            er_site="https://somewhere.buoyranger.org",
+            event_source="some_other_buoy_company",
+            er_event_type="gear_position",
+        ),
+    )
+
+
+@pytest.fixture
+def a_good_integration(a_good_state):
+    return IntegrationInformation(
+        id="00000000-0000-0000-0000-000000000000",
+        state=a_good_state.dict(),
+        enabled=True,
+        name="Test Integration",
+        endpoint="https://someplace.pamdas.orfg/api/v1.0",
+        login="test_login",
+        password="test_password",
+        token="test_token",
+    )
+
+
+@pytest.fixture
+def a_state_without_er_buoy_config():
+    return State(
+        er_token="super_secret_token",
+        er_site="fishing.pamdas.org",
+        event_source="fancy_buoy_company",
+        er_event_type="gear_position",
+        er_buoy_config=None,
+    )
+
+
+@pytest.fixture
+def an_integration_without_er_buoy_config(a_state_without_er_buoy_config):
+    return IntegrationInformation(
+        id="00000000-0000-0000-0000-000000000000",
+        state=a_state_without_er_buoy_config.dict(),
+        enabled=True,
+        name="Test Integration",
+        endpoint="https://someplace.pamdas.orfg/api/v1.0",
+        login="test_login",
+        password="test_password",
+        token="test_token",
+    )
+
+
+@pytest.fixture
+def a_good_configuration():
+    return {
+        "api_key": "anApiKey",
+        "rmw_url": "https://somermwhub.url",
+    }
+
+
+@pytest.fixture
+def mock_rmwhub_response():
+    return {
+        "format_version": 0.1,
+        "as_of_utc": "2024-11-06T20:53:21Z",
+        "updates": {
+            "description": 'New or changed locations of "others" gear',
+            "sets": [
+                {
+                    "set_id": "test_set_id_0",
+                    "deployment_type": "single",
+                    "traps": [
+                        {"sequence": 0, "latitude": -5.19816, "longitude": 122.8113}
+                    ],
+                },
+                {
+                    "set_id": "test_set_id_1",
+                    "deployment_type": "trawl",
+                    "traps": [
+                        {
+                            "sequence": 0,
+                            "latitude": 41.2576527,
+                            "longitude": -71.2768455,
+                        },
+                        {
+                            "sequence": 1,
+                            "latitude": 41.2544098,
+                            "longitude": -71.2793166,
+                        },
+                    ],
+                },
+                {
+                    "set_id": "test_set_id_2",
+                    "deployment_type": "single",
+                    "traps": [
+                        {
+                            "sequence": 0,
+                            "latitude": 41.3830183,
+                            "longitude": -71.5085983,
+                        }
+                    ],
+                },
+                {
+                    "set_id": "test_set_id_3",
+                    "deployment_type": "single",
+                    "traps": [
+                        {
+                            "sequence": 0,
+                            "latitude": 41.3830183,
+                            "longitude": -71.5085983,
+                        }
+                    ],
+                },
+                {
+                    "set_id": "test_set_id_4",
+                    "deployment_type": "trawl",
+                    "traps": [
+                        {
+                            "sequence": 0,
+                            "latitude": 41.1966817,
+                            "longitude": -71.3199314,
+                        },
+                        {
+                            "sequence": 1,
+                            "latitude": 41.193145,
+                            "longitude": -71.3223467,
+                        },
+                    ],
+                },
+            ],
+        },
+        "deletes": {
+            "sets": [
+                {"set_id": "test_delete_set_id_1"},
+                {"set_id": "test_delete_set_id_2"},
+                {"set_id": "test_delete_set_id_3"},
+                {"set_id": "test_delete_set_id_4"},
+                {"set_id": "test_delete_set_id_5"},
+            ]
+        },
+    }
+
+
+@pytest.fixture
+def mock_rmwhub_items():
+    return [
+        {
+            "set_id": "test_set_id_0",
+            "deployment_type": "single",
+            "traps": [{"sequence": 0, "latitude": -5.19816, "longitude": 122.8113}],
+        },
+        {
+            "set_id": "test_set_id_1",
+            "deployment_type": "trawl",
+            "traps": [
+                {"sequence": 0, "latitude": 41.2576527, "longitude": -71.2768455},
+                {"sequence": 1, "latitude": 41.2544098, "longitude": -71.2793166},
+            ],
+        },
+        {
+            "set_id": "test_set_id_2",
+            "deployment_type": "single",
+            "traps": [
+                {"sequence": 0, "latitude": 41.3830183, "longitude": -71.5085983}
+            ],
+        },
+        {
+            "set_id": "test_set_id_3",
+            "deployment_type": "single",
+            "traps": [
+                {"sequence": 0, "latitude": 41.3830183, "longitude": -71.5085983}
+            ],
+        },
+        {
+            "set_id": "test_set_id_4",
+            "deployment_type": "trawl",
+            "traps": [
+                {"sequence": 0, "latitude": 41.1966817, "longitude": -71.3199314},
+                {"sequence": 1, "latitude": 41.193145, "longitude": -71.3223467},
+            ],
+        },
+    ], [
+        {"set_id": "test_delete_set_id_1"},
+        {"set_id": "test_delete_set_id_2"},
+        {"set_id": "test_delete_set_id_3"},
+        {"set_id": "test_delete_set_id_4"},
+        {"set_id": "test_delete_set_id_5"},
+    ]
+
+
+@pytest.fixture
+def mock_rmw_observations():
+    return [
+        {
+            "name": "test_set_id_0_0",
+            "source": "rmwhub_test_set_id_0_0",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": -5.19816, "lon": 122.8113},
+            "additional": {
+                "subject_name": "test_set_id_0_0",
+                "rmwHub_id": "test_set_id_0",
+                "display_id": "test_display_hash_1",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {"latitude": "-5.19816", "longitude": "122.8113"},
+                        "device_id": "rmwhub_test_set_id_1_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    }
+                ],
+            },
+        },
+        {
+            "name": "test_set_id_1_0",
+            "source": "rmwhub_test_set_id_1_0",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": 41.2576527, "lon": -71.2768455},
+            "additional": {
+                "subject_name": "test_set_id_2_0",
+                "rmwHub_id": "test_set_id_1",
+                "display_id": "test_display_hash_1",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {
+                            "latitude": "41.2576527",
+                            "longitude": "-71.2768455",
+                        },
+                        "device_id": "rmwhub_test_set_id_1_0",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                    {
+                        "label": "b",
+                        "location": {
+                            "latitude": "41.2544098",
+                            "longitude": "-71.2793166",
+                        },
+                        "device_id": "rmwhub_test_set_id_1_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                ],
+            },
+        },
+        {
+            "name": "test_set_id_1_1",
+            "source": "rmwhub_test_set_id_1_1",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": 41.2544098, "lon": -71.2793166},
+            "additional": {
+                "subject_name": "test_set_id_2_1",
+                "rmwHub_id": "test_set_id_1",
+                "display_id": "test_display_hash_1",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {
+                            "latitude": "41.2576527",
+                            "longitude": "-71.2768455",
+                        },
+                        "device_id": "rmwhub_test_set_id_1_0",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                    {
+                        "label": "b",
+                        "location": {
+                            "latitude": "41.2544098",
+                            "longitude": "-71.2793166",
+                        },
+                        "device_id": "rmwhub_test_set_id_1_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                ],
+            },
+        },
+        {
+            "name": "test_set_id_2_0",
+            "source": "rmwhub_test_set_id_2_0",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": 41.3830183, "lon": -71.5085983},
+            "additional": {
+                "subject_name": "test_set_id_2_0",
+                "rmwHub_id": "test_set_id_2",
+                "display_id": "test_display_hash_2",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {
+                            "latitude": "41.3830183",
+                            "longitude": "-71.5085983",
+                        },
+                        "device_id": "rmwhub_test_set_id_2_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    }
+                ],
+            },
+        },
+        {
+            "name": "test_set_id_3_0",
+            "source": "rmwhub_test_set_id_3_0",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": 41.3830183, "lon": -71.5085983},
+            "additional": {
+                "subject_name": "test_set_id_3_0",
+                "rmwHub_id": "test_set_id_3",
+                "display_id": "test_display_hash_3",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {
+                            "latitude": "41.3830183",
+                            "longitude": "-71.5085983",
+                        },
+                        "device_id": "rmwhub_test_set_id_3_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    }
+                ],
+            },
+        },
+        {
+            "name": "test_set_id_4_0",
+            "source": "rmwhub_test_set_id_4_0",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": 41.1966817, "lon": -71.3199314},
+            "additional": {
+                "subject_name": "test_set_id_4_0",
+                "rmwHub_id": "test_set_id_4",
+                "display_id": "test_display_hash_4",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {
+                            "latitude": "41.1966817",
+                            "longitude": "-71.3199314",
+                        },
+                        "device_id": "rmwhub_test_set_id_4_0",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                    {
+                        "label": "b",
+                        "location": {
+                            "latitude": "41.193145",
+                            "longitude": "-71.3223467",
+                        },
+                        "device_id": "rmwhub_test_set_id_4_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                ],
+            },
+        },
+        {
+            "name": "test_set_id_4_1",
+            "source": "rmwhub_test_set_id_4_1",
+            "type": "ropeless_buoy",
+            "subject_type": "ropeless_buoy_device",
+            "recorded_at": "2024-10-22T13:33:15.081704-07:00",
+            "location": {"lat": 41.193145, "lon": -71.3223467},
+            "additional": {
+                "subject_name": "test_set_id_4_1",
+                "rmwHub_id": "test_set_id_4",
+                "display_id": "test_display_hash_4",
+                "event_type": "gear_deployed",
+                "devices": [
+                    {
+                        "label": "a",
+                        "location": {
+                            "latitude": "41.1966817",
+                            "longitude": "-71.3199314",
+                        },
+                        "device_id": "rmwhub_test_set_id_4_0",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                    {
+                        "label": "b",
+                        "location": {
+                            "latitude": "41.193145",
+                            "longitude": "-71.3223467",
+                        },
+                        "device_id": "rmwhub_test_set_id_4_1",
+                        "last_updated": "2024-10-22T13:33:15.081704-07:00",
+                    },
+                ],
+            },
+        },
+    ]
--- tests/test_rmwhub.py
+++ tests/test_rmwhub.py
@@ -1,3 +1,39 @@
 import pytest
+from unittest.mock import patch
 
-# TOOD: Add tests for testing the RmwHubAdapter
+
+@pytest.mark.asyncio
+async def test_rmwhub_download_data(mocker, mock_rmwhub_response, a_good_configuration):
+    """
+    Test rmwhub.download_data
+    """
+
+    # Setup mock_rmwhub_client
+    mock_rmwclient_search_others = mocker.patch("rmwhub.RmwHubClient.search_others")
+    mock_rmwclient_search_others.return_value = mock_rmwhub_response
+
+    from rmwhub import RmwHubAdapter
+
+    rmwadapter = RmwHubAdapter(
+        a_good_configuration.api_key, a_good_configuration.rmw_url
+    )
+    updates, deletes = rmwadapter.download_data(str(datetime.now().isoformat()))
+
+    assert len(updates) == 5
+    assert len(deletes) == 5
+
+
+async def test_rmwhub_process_updates(mocker, mock_rmw_items, mock_rmw_observations):
+    """
+    Test rmwhub.process_updates
+    """
+
+    from rmwhub import RmwHubAdapter
+
+    rmwadapter = RmwHubAdapter(
+        a_good_configuration.api_key, a_good_configuration.rmw_url
+    )
+    updates, deletes = mock_rmw_items
+    observations = rmwadapter.process_updates(updates)
+
+    assert len(observation) == 7
