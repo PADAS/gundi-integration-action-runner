6aef7d628a939d4faf8f2038ae8aac2c78f6de40
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2024-12-26 15:34:31 -0300

    Add batch generator in utils

--- app/services/utils.py
+++ app/services/utils.py
@@ -372,3 +372,8 @@ class UISchemaModelMixin:
             definitions.pop(field, None)
         json_schema_dict['definitions'] = definitions
         return json_schema_dict
+
+
+def generate_batches(iterable, batch_size):
+    for i in range(0, len(iterable), batch_size):
+        yield iterable[i: i + batch_size]
