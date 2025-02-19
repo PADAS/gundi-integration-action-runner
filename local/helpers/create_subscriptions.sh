437c706fefe8433c9905c256a69f21e5c4defe36
Chris Doehring <chrisdoehring@gmail.com>
2025-02-19 10:24:40 -0800

    Must pubsub initializers into a single container with a helper script.

--- local/helpers/create_subscriptions.sh
+++ local/helpers/create_subscriptions.sh
@@ -3,6 +3,9 @@
 # This script is used as part of automating a local development environment, particlarly for testing
 # sub-actions in an Action Runner.
 
+# This first operation is meant to wait until the PubSub emulator is up and running.
+until curl -X PUT http://pubsub_emulator:8085/v1/projects/local-project/topics/integration-events; do sleep 2; done
+
 # This createst the `local-actions-topic` topic. It's where the action runner is going to publish sub-actions.
 curl -X PUT http://pubsub_emulator:8085/v1/projects/local-project/topics/local-actions-topic
 
