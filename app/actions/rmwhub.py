6fa4bb4ae25f84e3eae489369aea1bd7322e4542
Douglas Poirier <douglaspoirier@Douglass-MacBook-Pro.local>
2025-04-04 18:54:28 -0600

    Changed share_with to Earth_Ranger

--- app/actions/rmwhub.py
+++ app/actions/rmwhub.py
@@ -894,7 +894,7 @@ class RmwHubAdapter:
             trawl_path="",
             # Share with is not set to ER because
             # we don't want our data back in the download step
-            share_with=None,
+            share_with=["Earth_Ranger"],
             traps=traps,
             when_updated_utc=datetime(
                 1970, 1, 1, 0, 0, 0, tzinfo=timezone.utc
