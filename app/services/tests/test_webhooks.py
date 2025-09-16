72ab2865fd84ff4c6883824350392734913de9e5
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-09-16 17:18:37 -0300

    Update unit tests

--- app/services/tests/test_webhooks.py
+++ app/services/tests/test_webhooks.py
@@ -3,6 +3,7 @@ import json
 from unittest.mock import ANY
 
 import pytest
+from unittest.mock import AsyncMock
 from fastapi.testclient import TestClient
 
 from app.conftest import MockWebhookPayloadModel, MockWebhookConfigModel
@@ -14,12 +15,12 @@ api_client = TestClient(app)
 
 @pytest.mark.asyncio
 async def test_process_webhook_request_with_fixed_schema(
-        mocker, integration_v2_with_webhook, mock_gundi_client_v2_for_webhooks, mock_publish_event,
+        mocker, integration_v2_with_webhook, mock_publish_event,
         mock_get_webhook_handler_for_fixed_json_payload, mock_webhook_handler,
         mock_webhook_request_headers_onyesha, mock_webhook_request_payload_for_fixed_schema
 ):
     mocker.patch("app.services.webhooks.get_webhook_handler", mock_get_webhook_handler_for_fixed_json_payload)
-    mocker.patch("app.services.webhooks._portal", mock_gundi_client_v2_for_webhooks)
+    mocker.patch("app.services.config_manager.IntegrationConfigurationManager.get_integration_details", AsyncMock(return_value=integration_v2_with_webhook))
 
     response = api_client.post(
         "/webhooks",
@@ -28,7 +29,6 @@ async def test_process_webhook_request_with_fixed_schema(
     )
 
     assert response.status_code == 200
-    assert mock_gundi_client_v2_for_webhooks.get_integration_details.called
     assert mock_get_webhook_handler_for_fixed_json_payload.called
     expected_payload = MockWebhookPayloadModel.parse_obj(mock_webhook_request_payload_for_fixed_schema)
     expected_config = MockWebhookConfigModel.parse_obj(integration_v2_with_webhook.webhook_configuration.data)
@@ -41,12 +41,12 @@ async def test_process_webhook_request_with_fixed_schema(
 
 @pytest.mark.asyncio
 async def test_process_webhook_request_with_dynamic_schema(
-        mocker, integration_v2_with_webhook_generic, mock_gundi_client_v2_for_webhooks_generic, mock_publish_event,
+        mocker, integration_v2_with_webhook_generic, mock_publish_event,
         mock_get_webhook_handler_for_generic_json_payload, mock_webhook_handler,
         mock_webhook_request_headers_onyesha, mock_webhook_request_payload_for_dynamic_schema
 ):
     mocker.patch("app.services.webhooks.get_webhook_handler", mock_get_webhook_handler_for_generic_json_payload)
-    mocker.patch("app.services.webhooks._portal", mock_gundi_client_v2_for_webhooks_generic)
+    mocker.patch("app.services.config_manager.IntegrationConfigurationManager.get_integration_details", AsyncMock(return_value=integration_v2_with_webhook_generic))
 
     response = api_client.post(
         "/webhooks",
@@ -55,7 +55,6 @@ async def test_process_webhook_request_with_dynamic_schema(
     )
 
     assert response.status_code == 200
-    assert mock_gundi_client_v2_for_webhooks_generic.get_integration_details.called
     assert mock_get_webhook_handler_for_generic_json_payload.called
     expected_config = GenericJsonTransformConfig.parse_obj(integration_v2_with_webhook_generic.webhook_configuration.data)
     mock_webhook_handler.assert_called_once_with(
